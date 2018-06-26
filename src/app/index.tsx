705bece731a009edca3b1f1052ae2a9970437ebf
MartynasP <39139245+RealFisherman@users.noreply.github.com>
2018-06-26 13:10:28 +0300

    added limit (cant go below 0), type changes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,56 +1,65 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-interface propscontracts { 
-    
- }
+interface Props {
 
- interface statecontracts {
+}
+
+interface State {
     count: number
- }
+}
 
-class App extends React.Component<propscontracts, statecontracts> {
-    constructor(props :propscontracts) {
+class App extends React.Component<Props, State> {
+    constructor(props: Props) {
         super(props);
         this.state = {
             count: 0
         }
     }
 
-    addOne() {
+    protected addOne: React.MouseEventHandler<HTMLButtonElement> = () => {
         this.setState({
             count: this.state.count + 1
         })
     }
 
-    minusOne() {
-        this.setState({
-            count: this.state.count - 1
-        })
+    protected minusOne: React.MouseEventHandler<HTMLButtonElement> = () => {
+        if (!(this.state.count == 0)) {
+            this.setState({
+                count: this.state.count - 1
+            })
+        }
     }
 
-    changeStartingValue(event: React.ChangeEvent<HTMLInputElement>) {
-        this.setState({
-            count: Number(event.target.value)
-        })
+    protected changeStartingValue: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+        if (Number(event.target.value) < 0) {
+            this.setState({
+                count: 0
+            })
+        }
+        else {
+            this.setState({
+                count: Number(event.target.value)
+            })
+        }
     }
 
-    resetCounter() {
+    protected resetCounter: React.MouseEventHandler<HTMLButtonElement> = () => {
         this.setState({
             count: 0
         })
     }
 
     public render(): JSX.Element {
-        return(
-         <div>
-             <div><input onChange= {(event: React.ChangeEvent<HTMLInputElement>) => this.changeStartingValue(event)} type="text" value={this.state.count}/></div>
-             <div>
-                 <button onClick={() => this.addOne()}>+</button>
-                 <button onClick={() => this.minusOne()}>-</button>
-             </div>
-             <div><button onClick={() => this.resetCounter()}>Reset</button></div>
-         </div>
+        return (
+            <div>
+                <div><input onChange={this.changeStartingValue} type="text" value={this.state.count} /></div>
+                <div>
+                    <button onClick={this.addOne}>+</button>
+                    <button onClick={this.minusOne}>-</button>
+                </div>
+                <div><button onClick={this.resetCounter}>Reset to 0</button></div>
+            </div>
         );
     }
 }
